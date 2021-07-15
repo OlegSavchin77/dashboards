c94dd83ea95774074d66f0664d292f31ed1c73cf
oleg <olegs@seetrue.ai>
2021-07-15 11:14:43 +0300

    .

--- test/html_dev_version_2.py
+++ test/html_dev_version_2.py
@@ -11,7 +11,7 @@ link = '[Please login to NEXUS ](http://nexus.int.seetrue.ai:8081/repository/dat
 st.markdown(link, unsafe_allow_html=True)
 
 # File Filter
-folder_path = folder_path = './'
+folder_path = folder_path = '.'
 filenames = [f for f in os.listdir(folder_path) if '.csv' in f]
 selected_filename = st.sidebar.selectbox('Select a file', filenames)
 csv_selected = os.path.join(folder_path, selected_filename)
